69b8bfb364ae5ea49119acdcae13d07552feab3e
Bryan <bryanmiessler@gmail.com>
2023-07-04 09:40:10 -0500

    iniciando repo

---
+++ array.js
@@ -0,0 +1,44 @@
+// const array = ["Diego", "Karen", "Oscar"];
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  delete(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+
+    return item;
+  }
+  shiftIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const myArray = new MyArray();
+myArray.push("Bryan");
+myArray.push("Laura");
+myArray.push("Duban");
+// console.log(myArray.get(1),"get");
+// console.log(myArray.pop(),"pop");
+console.log(myArray.delete(1),"delete");
+console.log(myArray);
